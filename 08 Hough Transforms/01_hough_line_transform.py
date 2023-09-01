f05e3438084c628c285a390ca30df34ae4e17a96
omerfarukgulhan <101866866+omerfarukgulhan@users.noreply.github.com>
2023-09-01 20:30:58 +0300

    Hough Transforms

---
+++ 08 Hough Transforms/01_hough_line_transform.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("gallery\h_line.png")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 75, 150)
+
+# cv2.HoughLines() uses too much cpu
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, maxLineGap=200)
+
+for line in lines:
+    print(line[0])
+    x1, y1, x2, y2 = line[0]
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
